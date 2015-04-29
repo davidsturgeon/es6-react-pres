c9184aaf93d146418be90c961aa3935d9c31f5a3
unknown <dsturgeon@LADSTURGEON.us.costar.local>
2015-04-29 16:08:19 -0700

    added uncached

--- completed/app.js
+++ completed/app.js
@@ -17,9 +17,14 @@ app.use(mount('/fa', serve('../node_modules/font-awesome')));
 app.use(mount('/public', serve('./public')));
 
 app.use(route.get('/', function *() {
-  console.log('rendering');
+  var ClientApp = requireUncached('./jsx/index.jsx'); // dont do this in production. I just want to be able to show changes. 
   var rendered = React.renderToString(React.createElement(ClientApp));
   this.body = _.template(baseTemplate)({body:rendered});
 }));
 
 app.listen(3000);
+
+function requireUncached(module){
+    delete require.cache[require.resolve(module)]
+    return require(module)
+}
